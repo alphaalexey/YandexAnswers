20d7a753472fe35e4cc472e3724fe9d97e0fae1f
alphaalexey <homyakovost@gmail.com>
2020-12-09 09:49:08 +0700

    Updates

---
+++ precache-manifest.e9c9c1c756c02467954e448119c3cb73.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b4c937ac3db27a4a5e9ca22719b40c",
+    "url": "/YandexAnswers/index.html"
+  },
+  {
+    "revision": "b587c5b8aa310e3a85bf",
+    "url": "/YandexAnswers/static/css/2.cdee455b.chunk.css"
+  },
+  {
+    "revision": "b9bd9f02ce8f451fd1d8",
+    "url": "/YandexAnswers/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "b587c5b8aa310e3a85bf",
+    "url": "/YandexAnswers/static/js/2.8a6ad27e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/YandexAnswers/static/js/2.8a6ad27e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9bd9f02ce8f451fd1d8",
+    "url": "/YandexAnswers/static/js/main.cbb91e00.chunk.js"
+  },
+  {
+    "revision": "e6e989c7d3fad62a3ec9",
+    "url": "/YandexAnswers/static/js/runtime-main.067e005d.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/YandexAnswers/static/media/persik.4e1ec840.png"
+  }
+]);
